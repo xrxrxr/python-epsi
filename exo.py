dbe35af682debac10e655813139a6a8509a87e6f
xavier Re4 <xrthefrench@gmail.com>
2021-01-11 12:28:09 +0100

    not working

--- exo.py
+++ exo.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
-import csv
+# -*- coding: utf-8 -*-
+##Iso-8859-1
 import os
+import csv
+
+
+# Parent Directory path 
+parent_dir = "~/projets/python-epsi"
+# Directory 
+directory = "python-epsi"
+# Path 
+path = os.path.join(parent_dir, directory) 
+
+
 with open('departements-france.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ',
                             quotechar='|')
     for row in spamreader:
         print(', '.join(row))
-    for each in spamreader:
-        mdkir(', '.join(row))
+
+  
+if not os.path.exists(', '.join(row)):
+            os.makedirs(', '.join(row))
+            print("create folder " + ', '.join(row))
+
+
